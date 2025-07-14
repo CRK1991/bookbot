be32b66f24f5952de5b6878e91b4f27b81efc812
CRK1991 <SCYCK1@NOTTINGHAM.AC.UK>
2025-07-14 11:51:54 +0100

    added a character count dictionary in stat.py that imports into main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Bookbot project
 
 from stats import count_words # importing the count_words function
+from stats import char_count # importing the char_count function
 
 filepath = "books/frankenstein.txt"
 
@@ -11,8 +12,9 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text(filepath)
     num_words = count_words(book_text)
+    num_chars = char_count(book_text)
     print(f"{num_words} words found in the document")
-    # print(book_text)
+    print(f"{num_chars} characters found in the document")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    char_dict = {}
+
+    no_caps_text = text.lower()
+    for char in no_caps_text:
+        if char.isalpha():
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+
+    return char_dict
