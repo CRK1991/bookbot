273e3a5b06d941f46651f189d41f7133f9118cb3
CRK1991 <SCYCK1@NOTTINGHAM.AC.UK>
2025-07-14 11:32:16 +0100

    Full printout of the frankenstein book

---
+++ main.py
@@ -0,0 +1,14 @@
+# Bookbot project
+
+filepath = "books/frankenstein.txt"
+
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        return file.read()
+
+def main():
+    book_text = get_book_text(filepath)
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
