171680fe59e7a9e615817bedbaa19bf7830b9ec8
CRK1991 <SCYCK1@NOTTINGHAM.AC.UK>
2025-07-18 08:53:28 +0100

    Added sorted character count

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from stats import count_words # importing the count_words function
 from stats import char_count # importing the char_count function
+from stats import sort_char_count # importing the sort_char_count function
 
 filepath = "books/frankenstein.txt"
 
@@ -13,8 +14,14 @@ def main():
     book_text = get_book_text(filepath)
     num_words = count_words(book_text)
     num_chars = char_count(book_text)
-    print(f"{num_words} words found in the document")
-    print(f"{num_chars} characters found in the document")
-
+    sorted_char_count = sort_char_count(num_chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_count:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -13,3 +13,11 @@ def char_count(text):
             char_dict[char] = 1
 
     return char_dict
+
+def sort_char_count(char_dict):
+    char_list = []
+    for k, v in char_dict.items():
+        if k.isalpha():
+            char_list.append({"char": k, "num": v})
+    char_list.sort(key=lambda item: item["num"], reverse=True)
+    return char_list
