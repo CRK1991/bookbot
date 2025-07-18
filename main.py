7144b549eacb7693165f0c886d8443f51bce6572
CRK1991 <SCYCK1@NOTTINGHAM.AC.UK>
2025-07-18 09:17:57 +0100

    Modified Bookbot to allow for analysis of any .txt file present in the /books directory

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # Bookbot project
+import sys
 
 from stats import count_words # importing the count_words function
 from stats import char_count # importing the char_count function
 from stats import sort_char_count # importing the sort_char_count function
 
-filepath = "books/frankenstein.txt"
+# Ensure the script is run with a file path argument
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
@@ -16,7 +22,7 @@ def main():
     num_chars = char_count(book_text)
     sorted_char_count = sort_char_count(num_chars)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
